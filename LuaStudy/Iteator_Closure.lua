ac18059d02254f24ea5feeda3597448b41dcda64
yaoweixin <3344565781@qq.com>
2023-03-30 17:15:22 +0800

    Update Iteator_Closure.lua

--- LuaStudy/Iteator_Closure.lua
+++ LuaStudy/Iteator_Closure.lua
@@ -51,3 +51,67 @@ for word in allWord() do
     print(word)
 end  
 
+--for 结构的第一步是求解 in 后面的表达式。这些表达式应该返回 for 所需的三个值：迭代器函数、不变状态和控制变量的初始值
+--我们在上述的for循环中使用的是自己定义的迭代器，本质上来说其实我们的迭代器都是返回三个值，在上个函数中，不变状态相当于line，pos其实就相当于控制变量，仅仅用于初始化的时候获得这个值
+--然后我们执行迭代器函数，执行迭代器函函数其实就是传入我们的不变状态，和控制变量初始化的值
+
+--[[
+    for <var-list> in <exp-list> do
+      <body>
+    end
+]]
+
+--执行过程等价于
+--for var_1, ..., var_n in explist do block end
+--[[
+    do
+      local _f, _s, _var = explist
+      while true do
+        local var_1, ... , var_n = _f(_s, _var)
+        _var = var_1
+        if _var == nil then break end
+        block
+      end
+    end
+]]
+
+
+--------------------------------------------------------------
+
+
+--接着我们来说说无状态迭代器（不会产生新的闭包）
+
+a = {111,222,333}
+
+for index, value in ipairs(t) do
+    print(index,value)
+end
+
+--接着来看看ipairs的底层实现
+
+function iter(aa,i)
+    i = i + 1
+    local v = aa[i]
+    if v then
+        return i,v
+    end
+end
+--当我们调用ipairs的时候，会返回三个值，迭代器函数、不变状态和控制变量的初始值
+--在这里面分别就是iter,tt(table),0
+--接着开始调用iter（tt,0）,返回1，tt[1]，直到返回num,nil结束
+function ipairs(tt)
+    return iter,tt,0
+end
+
+--pairs同理
+
+function pairs(tt)
+    return next,tt,0
+end
+
+--同时也可以这样调用
+for key, value in pairs(t) do
+    print(value)
+end
+
+
