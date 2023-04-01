1c061662eb2b335b75230d663e5472aaa74a64c5
yaoweixin <3344565781@qq.com>
2023-04-01 22:48:01 +0800

    Update Default_value.lua

--- LuaStudy/Default_value.lua
+++ LuaStudy/Default_value.lua
@@ -16,5 +16,37 @@ print(tab.a,tab.b)
 local mt2 = {__index = function (t) return t.__ end}
 function setDefault2(t,d)
     t.__ = d
-    setmetatable(t,mt)    
+    setmetatable(t,mt2)    
 end
+
+tab2 = {x = 10,y = 10}
+tab3 = {x = 12,y = 13}
+--这样我们每次在寻找不存在的值的时候，我们会去查找元表有没有__index方法，
+--发现__index方法对应的是一个函数，我们会去调用这个函数，并且产生返回值
+--我们使用setfault2的目的是，让我们每个表格调用这个方法后，表格自身都会加上__属性
+--然后用一个统一的元表，当我们访问不存在的键的时候，就返回我们的"__"值
+print(tab2.x,tab2.z)
+setDefault2(tab2,23)
+setDefault2(tab3,12)
+print(tab2.x,tab2.z)
+print(tab3.x,tab3.z)
+
+--------------------------------------------------------------------
+--接着来看看解决默认值的键相同的问题(和我们自身table里面的键名称相同)
+
+local key = {}
+local mt3 = {__index = function (t) return t[key] end}
+
+function setDefault3(t,d)
+    t[key] = d
+    setmetatable(t,mt3)
+end
+--我们默认值的key不应该和我们表格里面的其他元素起冲突，所以就使用了table作为key，独一无二
+
+tabb2 = {x = 10,y = 10}
+tabb3 = {x = 12,y = 13}
+print(tabb2.x,tabb2.z)
+setDefault3(tabb2,23)
+setDefault3(tabb3,12)
+print(tabb2.x,tabb2.z)
+print(tabb3.x,tabb3.z)
