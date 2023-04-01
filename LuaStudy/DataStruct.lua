c3ba7cb46d5cdba0596c32051ea0ef4981d2c97d
yaoweixin <3344565781@qq.com>
2023-04-02 00:12:23 +0800

    Update DataStruct.lua

--- LuaStudy/DataStruct.lua
+++ LuaStudy/DataStruct.lua
@@ -54,8 +54,56 @@ while l do
 end
 
 --------------------------------------------
---让我们来实现一个队列
+--让我们来实现一个双向队列捏
 List = {}
 function List.new ()
   return {first = 0, last = -1}
 end
+
+function List.pushleft (list, value)
+    local first = list.first - 1
+    list.first = first
+    list[first] = value
+end
+
+function List.pushright (list, value)
+    local last = list.last + 1
+    list.last = last
+    list[last] = value
+end
+
+function List.popleft (list)
+    local first = list.first
+    if first > list.last then error("list is empty") end
+    local value = list[first]
+    list[first] = nil        -- to allow garbage collection
+    list.first = first + 1
+    return value
+end
+
+function List.popright (list)
+    local last = list.last
+    if list.first > last then error("list is empty") end
+    local value = list[last]
+    list[last] = nil         -- to allow garbage collection
+    list.last = last - 1
+    return value
+end
+
+------------------------------------------------------
+--让我们实现一个集合
+--讲下lua当中set的定义
+--In Lua, an efficient and simple way to represent such sets is to put the set elements as indices in a table. 
+--Then, instead of searching the table for a given element, you just index the table and test whether the result is nil or not. 
+reserved = {
+    ["while"] = true,     ["end"] = true,
+    ["function"] = true,  ["local"] = true,
+}
+
+function Set (list)
+    local set = {}
+    for _, l in ipairs(list) do set[l] = true end
+    return set
+end
+
+reserved = Set{"while", "end", "function", "local", }
