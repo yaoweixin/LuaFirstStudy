1d7c8415f73484ba94cf1dd94d28d115e2bb0801
yaoweixin <3344565781@qq.com>
2023-04-04 10:35:49 +0800

    Update Require_script.lua

--- LuaStudy/Require_script.lua
+++ LuaStudy/Require_script.lua
@@ -1,5 +1,5 @@
 --require("module1")
-require("complex")
+require("complex") --我们同样也可以用一个局部变量来接收我们脚本的返回值  local complex = require("complex")
 --可以输出我们这个lua文件已经调用的文件内容
 --这些文件和key放在了package,loaded表下面
 for k, value in pairs(package.loaded) do
@@ -34,5 +34,24 @@ f = loadfile("C:/Users/Admin/Desktop/LuaFirstStudy/LuaStudy/FirstLua.lua")
 f() --在require中我们会自动执行，并将其存入表中，这样下一次就不用再调用了
 print(c,d)
 
+
+
 --我们希望require进来的模块是不允许定义全局变量的，因为全局变量会污染我们整个环境，并可能造成意想不到的后果，
 --在lua 5.1，我们可以使用setfenv函数来设置函数环境，而在lua 5.2以上版本，则可以通过修改env参数来解决
+
+---------------------------------------------------------------------
+
+--一个lua文件里只能有一个retrun，但是一个return里面可以返回多个模块
+--把我们的模块全都定义为局部有个好处就是：
+--这样我们运行别的文件的时候不会污染我们的全局环境表，使用局部变量，减少全局变量的使用
+local modules = require("Module_local")
+
+modules[1].module1_print1()
+modules[2].module2_print2()
+---------------------------------------------------------------
+--lua里的赋值是浅拷贝！！！一个改了另一个也跟着改
+local stu = require("InitClass")
+stu.id = 10086
+print(student_A.id,stu.id)
+-----------------------------------------------------------------------
+
