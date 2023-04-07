81e66017998b3c464e9b4ac50e881bbbb4732177
yaoweixin <3344565781@qq.com>
2023-04-07 17:10:36 +0800

    Update Class_test.lua

--- LuaStudy/Lua_Class/Class_test.lua
+++ LuaStudy/Lua_Class/Class_test.lua
@@ -2,6 +2,30 @@ package.path = "C:\\Users\\Admin\\Desktop\\LuaFirstStudy\\LuaStudy\\Lua_Class\\?
 
 require("Class_create")
 
+--基类的实现
+local Animal = class("Animal")
+
+function Animal:ctor()    
+end
+
+function Animal:AMI()    
+end
+
+--继承类的实现
+local Dog = class("Dog",Animal)
+
+function Dog:ctor(weight,height)
+    self.weight = weight
+    self.height = height
+end
+
+function Dog:AMI()
+    return self.weight / self.height
+end
+
+
+
+
 local Shape = class("Shape")
 
 function Shape:ctor()
@@ -29,13 +53,36 @@ function Square:ctor(s)
     Rect.ctor(self,s,s)
 end
 
+--多继承实现
+local Dog_Square = class("Dog_Square",Dog,Square)
+
+function Dog_Square:ctor(weight,height,w)
+    Dog.ctor(self,weight,height)
+    Square.ctor(self,w)
+end
+
+local Dog_Rect = class("Dog_Rect",Dog,Rect)
+
+function Dog_Rect:ctor(weight,height,w)
+    Dog.ctor(self,weight,height)
+    Rect.ctor(self,w)
+end
+
 function test()
+    --这三个都是单继承，其中square体现了继承
     local s = Shape.new()
     local r = Rect.new(5,8)
     local Squ = Square.new(3)
     print(s:size())
     print(r:size())
     print(Squ:size())
+
+    --这两个是多继承
+    local ds = Dog_Square.new(12,13,4)
+    local dr = Dog_Rect.new()
+    --但是我们要在构造函数手动赋值，挺麻烦的
+    print(ds:size() .. " " .. ds:AMI())
+    print(dr:size() .. " " .. dr:AMI())
 end
 
 test()
